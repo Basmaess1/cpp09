d71a710f1d99f6f0838977bcc76e5794d6b64c9a
Basma Essabri <bessabri@c1r8p7.1337.ma>
2026-09-03 16:03:27 +0100

    vect

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -10,8 +10,12 @@ PmergeMe::PmergeMe(const PmergeMe& other)
 }
 PmergeMe& PmergeMe::operator=(const PmergeMe& other)
 {
-    if(this != &other)
-        *this = other;
+    if (this != &other)
+    {
+        vec = other.vec;
+        deq = other.deq;
+    }
+
     return *this;
 }
 
@@ -72,45 +76,21 @@ bool PmergeMe::ParseNumber(const std::string& str, int &number) const
     return true;
 }
 
-void PmergeMe::makePairsVector()
-{
-    std::vector<Pair> pairs;
-
-    std::size_t i = 0;
-
-    while (i + 1 < vec.size())
-    {
-        Pair p;
-
-        if (vec[i] < vec[i + 1])
-        {
-            p.small = vec[i];
-            p.large = vec[i + 1];
-        }
-        else
-        {
-            p.small = vec[i + 1];
-            p.large = vec[i];
-        }
-        pairs.push_back(p);
-
-        i += 2;
-    }
-    // to print
-    std::vector<Pair>::iterator it;
-
-    for (it = pairs.begin(); it != pairs.end(); ++it)
-    {
-        std::cout << "(" << it->small
-              << ", " << it->large << ") ";
-    }
-    std::cout << std::endl;
-}
 
 void PmergeMe::fordJohnsonVector(std::vector<int> &numbers)
 {
     if (numbers.size() <= 1)
         return;
+
+    bool hasStraggler = (numbers.size() % 2 != 0);
+    int straggler = 0;
+
+    if (hasStraggler)
+        straggler = numbers[numbers.size() - 1];
+
+    // -----------------------------------
+    // 1. Make pairs
+    // -----------------------------------
 
     std::vector<Pair> pairs;
 
@@ -135,34 +115,26 @@ void PmergeMe::fordJohnsonVector(std::vector<int> &numbers)
         i += 2;
     }
 
-    /*
-     * Put the large elements into a separate vector.
-     */
+    // -----------------------------------
+    // 2. Extract large elements
+    // -----------------------------------
+
     std::vector<int> largeElements;
 
     for (i = 0; i < pairs.size(); ++i)
         largeElements.push_back(pairs[i].large);
 
-    /*
-     * If numbers.size() is odd,
-     * the last element has no pair.
-     */
-    bool hasStraggler = (numbers.size() % 2 != 0);
+    // -----------------------------------
+    // 3. Recursively sort large elements
+    // -----------------------------------
 
-    int straggler = 0;
-
-    if (hasStraggler)
-        straggler = numbers[numbers.size() - 1];
-
-    /*
-     * Recursively sort the large elements.
-     */
     fordJohnsonVector(largeElements);
 
-    /*
-     * Reorder our pairs according to the
-     * sorted large elements.
-     */
+    // -----------------------------------
+    // 4. Reorder pairs according
+    //    to sorted large elements
+    // -----------------------------------
+
     std::vector<Pair> sortedPairs;
 
     for (i = 0; i < largeElements.size(); ++i)
@@ -177,114 +149,95 @@ void PmergeMe::fordJohnsonVector(std::vector<int> &numbers)
         }
     }
 
-    /*
-     * Main chain starts with the sorted large elements.
-     */
-    numbers.clear();
+    // -----------------------------------
+    // 5. Build main chain
+    // -----------------------------------
+
+    std::vector<int> mainChain;
 
     for (i = 0; i < largeElements.size(); ++i)
-        numbers.push_back(largeElements[i]);
+        mainChain.push_back(largeElements[i]);
 
-    /*
-     * Insert the small elements.
-     *
-     * For now, we simply insert them in order.
-     * We will replace this part with the
-     * Jacobsthal insertion later.
-     */
-    for (i = 0; i < sortedPairs.size(); ++i)
+    // -----------------------------------
+    // 6. Insert b1
+    // -----------------------------------
+
+    if (!sortedPairs.empty())
+        mainChain.insert(
+            mainChain.begin(),
+            sortedPairs[0].small
+        );
+
+    // -----------------------------------
+    // 7. Insert remaining b's
+    //    using Jacobsthal order
+    // -----------------------------------
+
+    std::size_t pendingCount = 0;
+
+    if (sortedPairs.size() > 1)
+        pendingCount = sortedPairs.size() - 1;
+
+    std::vector<std::size_t> order =
+        makeJacobsthalOrder(pendingCount);
+
+    for (i = 0; i < order.size(); ++i)
     {
-        std::vector<int>::iterator pos = numbers.begin();
+        std::size_t index = order[i];
 
-        while (pos != numbers.end() && *pos < sortedPairs[i].small)
-            ++pos;
-
-        numbers.insert(pos, sortedPairs[i].small);
+        binaryInsertVector(
+            mainChain,
+            sortedPairs[index - 1].small,
+            sortedPairs[index - 1].large
+        );
     }
 
-    /*
-     * Insert the unpaired element.
-     */
+    // -----------------------------------
+    // 8. Insert straggler
+    // -----------------------------------
+
     if (hasStraggler)
     {
-        std::vector<int>::iterator pos = numbers.begin();
+        std::vector<int>::iterator begin = mainChain.begin();
+        std::vector<int>::iterator end = mainChain.end();
 
-        while (pos != numbers.end() && *pos < straggler)
-            ++pos;
+        while (begin < end)
+        {
+            std::vector<int>::iterator middle =
+                begin + (end - begin) / 2;
 
-        numbers.insert(pos, straggler);
+            if (*middle < straggler)
+                begin = middle + 1;
+            else
+                end = middle;
+        }
+
+        mainChain.insert(begin, straggler);
     }
+
+    numbers = mainChain;
 }
-
-// void PmergeMe::fordJohnsonVector(std::vector<Pair> &pairs)
-// {
-//     if (pairs.size() <= 1)
-//         return;
-
-//     std::vector<Pair> nextPairs;
-
-//     std::size_t i = 0;
-
-//     while (i + 1 < pairs.size())
-//     {
-//         Pair p;
-
-//         if (pairs[i].large < pairs[i + 1].large)
-//         {
-//             p.small = pairs[i].large;
-//             p.large = pairs[i + 1].large;
-//         }
-//         else
-//         {
-//             p.small = pairs[i + 1].large;
-//             p.large = pairs[i].large;
-//         }
-
-//         nextPairs.push_back(p);
-//         i += 2;
-//     }
-
-//     fordJohnsonVector(nextPairs);
-
-//     std::vector<Pair> sortedPairs;
-
-//     for (std::size_t j = 0; j < nextPairs.size(); ++j)
-//     {
-//         for (std::size_t k = 0; k < pairs.size(); ++k)
-//         {
-//             if (pairs[k].large == nextPairs[j].large)
-//             {
-//                 sortedPairs.push_back(pairs[k]);
-//                 break;
-//             }
-//         }
-//     }
-
-//     if (pairs.size() % 2 != 0)
-//     {
-//         sortedPairs.push_back(pairs[pairs.size() - 1]);
-//     }
-
-//     pairs = sortedPairs;
-// }
-
 std::vector<std::size_t> PmergeMe::makeJacobsthalOrder(
-    std::size_t count) const
+    std::size_t pendingCount) const
 {
     std::vector<std::size_t> order;
 
     std::size_t previous = 1;
     std::size_t current = 3;
 
-    while (previous < count)
+    std::size_t start = 2;
+
+    while (start <= pendingCount + 1)
     {
         std::size_t end = current;
 
-        if (end > count)
-            end = count;
+        if (end > pendingCount + 1)
+            end = pendingCount + 1;
 
-        for (std::size_t i = end; i > previous; --i)
+        for (std::size_t i = end; i >= start; --i)
             order.push_back(i);
+
+        start = current + 1;
 
         std::size_t next = current + 2 * previous;
         previous = current;
@@ -293,21 +246,22 @@ std::vector<std::size_t> PmergeMe::makeJacobsthalOrder(
 
     return order;
 }
-void PmergeMe::binaryInsertVector(std::vector<int> &mainChain,
-                                   int value,
-                                   int limit)
+
+
+
+void PmergeMe::binaryInsertVector(
+    std::vector<int> &mainChain,
+    int value,
+    int limit)
 {
     std::vector<int>::iterator begin = mainChain.begin();
-    std::vector<int>::iterator end = mainChain.end();
+    std::vector<int>::iterator end = mainChain.begin();
 
-    if (limit != 2147483647)
-    {
-        end = mainChain.begin();
+    // Search only before the partner.
+    while (end != mainChain.end() && *end != limit)
+        ++end;
 
-        while (end != mainChain.end() && *end != limit)
-            ++end;
-    }
-
+    // Binary search.
     while (begin < end)
     {
         std::vector<int>::iterator middle =
@@ -326,6 +280,76 @@ void PmergeMe::sortVector()
 {
     fordJohnsonVector(vec);
 }
+
+
+void PmergeMe::printAfterVector() const
+{
+    std::vector<int>::const_iterator it;
+
+    std::cout << "After: ";
+
+    for (it = vec.begin(); it != vec.end(); ++it)
+        std::cout << *it << " ";
+
+    std::cout << std::endl;
+}
+
+// std::vector<std::size_t> PmergeMe::makeJacobsthalOrder(
+//     std::size_t count) const
+// {
+//     std::vector<std::size_t> order;
+
+//     std::size_t previous = 1;
+//     std::size_t current = 3;
+
+//     while (previous < count)
+//     {
+//         std::size_t end = current;
+
+//         if (end > count)
+//             end = count;
+
+//         for (std::size_t i = end; i > previous; --i)
+//             order.push_back(i);
+
+//         std::size_t next = current + 2 * previous;
+//         previous = current;
+//         current = next;
+//     }
+
+//     return order;
+// }
+
+
+// void PmergeMe::binaryInsertVector(std::vector<int> &mainChain,
+//                                    int value,
+//                                    int limit)
+// {
+//     std::vector<int>::iterator begin = mainChain.begin();
+//     std::vector<int>::iterator end = mainChain.end();
+
+//     if (limit != 2147483647)
+//     {
+//         end = mainChain.begin();
+
+//         while (end != mainChain.end() && *end != limit)
+//             ++end;
+//     }
+
+//     while (begin < end)
+//     {
+//         std::vector<int>::iterator middle =
+//             begin + (end - begin) / 2;
+
+//         if (*middle < value)
+//             begin = middle + 1;
+//         else
+//             end = middle;
+//     }
+
+//     mainChain.insert(begin, value);
+// }
+
 // void PmergeMe::sortVector()
 // {
 //     std::vector<Pair> pairs;
@@ -383,14 +407,200 @@ void PmergeMe::sortVector()
 //     vec = mainChain;
 // }
 
-void PmergeMe::printAfterVector() const
-{
-    std::vector<int>::const_iterator it;
+// void PmergeMe::fordJohnsonVector(std::vector<int> &numbers)
+// {
+//     if (numbers.size() <= 1)
+//         return;
 
-    std::cout << "After: ";
+//     std::vector<Pair> pairs;
 
-    for (it = vec.begin(); it != vec.end(); ++it)
-        std::cout << *it << " ";
+//     std::size_t i = 0;
 
-    std::cout << std::endl;
-}
+//     while (i + 1 < numbers.size())
+//     {
+//         Pair p;
+
+//         if (numbers[i] < numbers[i + 1])
+//         {
+//             p.small = numbers[i];
+//             p.large = numbers[i + 1];
+//         }
+//         else
+//         {
+//             p.small = numbers[i + 1];
+//             p.large = numbers[i];
+//         }
+
+//         pairs.push_back(p);
+//         i += 2;
+//     }
+
+//     /*
+//      * Put the large elements into a separate vector.
+//      */
+//     std::vector<int> largeElements;
+
+//     for (i = 0; i < pairs.size(); ++i)
+//         largeElements.push_back(pairs[i].large);
+
+//     /*
+//      * If numbers.size() is odd,
+//      * the last element has no pair.
+//      */
+//     bool hasStraggler = (numbers.size() % 2 != 0);
+
+//     int straggler = 0;
+
+//     if (hasStraggler)
+//         straggler = numbers[numbers.size() - 1];
+
+//     /*
+//      * Recursively sort the large elements.
+//      */
+//     fordJohnsonVector(largeElements);
+
+//     /*
+//      * Reorder our pairs according to the
+//      * sorted large elements.
+//      */
+//     std::vector<Pair> sortedPairs;
+
+//     for (i = 0; i < largeElements.size(); ++i)
+//     {
+//         for (std::size_t j = 0; j < pairs.size(); ++j)
+//         {
+//             if (pairs[j].large == largeElements[i])
+//             {
+//                 sortedPairs.push_back(pairs[j]);
+//                 break;
+//             }
+//         }
+//     }
+
+//     /*
+//      * Main chain starts with the sorted large elements.
+//      */
+//     numbers.clear();
+
+//     for (i = 0; i < largeElements.size(); ++i)
+//         numbers.push_back(largeElements[i]);
+
+//     /*
+//      * Insert the small elements.
+//      *
+//      * For now, we simply insert them in order.
+//      * We will replace this part with the
+//      * Jacobsthal insertion later.
+//      */
+//     for (i = 0; i < sortedPairs.size(); ++i)
+//     {
+//         std::vector<int>::iterator pos = numbers.begin();
+
+//         while (pos != numbers.end() && *pos < sortedPairs[i].small)
+//             ++pos;
+
+//         numbers.insert(pos, sortedPairs[i].small);
+//     }
+
+//     /*
+//      * Insert the unpaired element.
+//      */
+//     if (hasStraggler)
+//     {
+//         std::vector<int>::iterator pos = numbers.begin();
+
+//         while (pos != numbers.end() && *pos < straggler)
+//             ++pos;
+
+//         numbers.insert(pos, straggler);
+//     }
+// }
+
+// void PmergeMe::fordJohnsonVector(std::vector<Pair> &pairs)
+// {
+//     if (pairs.size() <= 1)
+//         return;
+
+//     std::vector<Pair> nextPairs;
+
+//     std::size_t i = 0;
+
+//     while (i + 1 < pairs.size())
+//     {
+//         Pair p;
+
+//         if (pairs[i].large < pairs[i + 1].large)
+//         {
+//             p.small = pairs[i].large;
+//             p.large = pairs[i + 1].large;
+//         }
+//         else
+//         {
+//             p.small = pairs[i + 1].large;
+//             p.large = pairs[i].large;
+//         }
+
+//         nextPairs.push_back(p);
+//         i += 2;
+//     }
+
+//     fordJohnsonVector(nextPairs);
+
+//     std::vector<Pair> sortedPairs;
+
+//     for (std::size_t j = 0; j < nextPairs.size(); ++j)
+//     {
+//         for (std::size_t k = 0; k < pairs.size(); ++k)
+//         {
+//             if (pairs[k].large == nextPairs[j].large)
+//             {
+//                 sortedPairs.push_back(pairs[k]);
+//                 break;
+//             }
+//         }
+//     }
+
+//     if (pairs.size() % 2 != 0)
+//     {
+//         sortedPairs.push_back(pairs[pairs.size() - 1]);
+//     }
+
+//     pairs = sortedPairs;
+// }
+
+// ========to debug============
+
+// void PmergeMe::makePairsVector()
+// {
+//     std::vector<Pair> pairs;
+
+//     std::size_t i = 0;
+
+//     while (i + 1 < vec.size())
+//     {
+//         Pair p;
+
+//         if (vec[i] < vec[i + 1])
+//         {
+//             p.small = vec[i];
+//             p.large = vec[i + 1];
+//         }
+//         else
+//         {
+//             p.small = vec[i + 1];
+//             p.large = vec[i];
+//         }
+//         pairs.push_back(p);
+
+//         i += 2;
+//     }
+//     // to print
+//     std::vector<Pair>::iterator it;
+
+//     for (it = pairs.begin(); it != pairs.end(); ++it)
+//     {
+//         std::cout << "(" << it->small
+//               << ", " << it->large << ") ";
+//     }
+//     std::cout << std::endl;
+// }
